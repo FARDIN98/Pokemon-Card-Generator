73a1a9aab08b0e3b013f831361cd5c3e2d12b420
FARDIN98 <fardinahmed66@gmail.com>
2023-06-08 14:05:38 +0600

    comments added

--- script.js
+++ script.js
@@ -1,66 +1,58 @@
-
 const typeColor = {
-    bug: "#26de81",
-    dragon: "#ffeaa7",
-    electric: "#fed330",
-    fairy: "#FF0069",
-    fighting: "#30336b",
-    fire: "#f0932b",
-    flying: "#81ecec",
-    grass: "#00b894",
-    ground: "#EFB549",
-    ghost: "#a55eea",
-    ice: "#74b9ff",
-    normal: "#95afc0",
-    poison: "#6c5ce7",
-    psychic: "#a29bfe",
-    rock: "#2d3436",
-    water: "#0190FF",
-}
-const url = "https://pokeapi.co/api/v2/pokemon/";
-const card = document.getElementById("card");
-const btn = document.getElementById("btn");
+    bug: "#26de81", // Color code for bug type
+    dragon: "#ffeaa7", // Color code for dragon type
+    electric: "#fed330", // Color code for electric type
+    fairy: "#FF0069", // Color code for fairy type
+    fighting: "#30336b", // Color code for fighting type
+    fire: "#f0932b", // Color code for fire type
+    flying: "#81ecec", // Color code for flying type
+    grass: "#00b894", // Color code for grass type
+    ground: "#EFB549", // Color code for ground type
+    ghost: "#a55eea", // Color code for ghost type
+    ice: "#74b9ff", // Color code for ice type
+    normal: "#95afc0", // Color code for normal type
+    poison: "#6c5ce7", // Color code for poison type
+    psychic: "#a29bfe", // Color code for psychic type
+    rock: "#2d3436", // Color code for rock type
+    water: "#0190FF", // Color code for water type
+};
+
+const url = "https://pokeapi.co/api/v2/pokemon/"; // URL for the PokeAPI
+const card = document.getElementById("card"); // Get the card element
+const btn = document.getElementById("btn"); // Get the button element
 
 let getPokeData = () => {
-    // Generate a random number between 1 and 150
-    let id = Math.floor(Math.random() * 150) + 1;
-    console.log(id);
-    // Combine the pokeapi url with pokemon id
-    const finalUrl = url + id;
-    console.log(finalUrl);
-    // Fetch generated url
-    fetch(finalUrl)
-    .then( (response) => response.json() )
-    .then( (data) => {
-        generateCard(data);
-    })
-}
-
-// Generate Card
+    let id = Math.floor(Math.random() * 150) + 1; // Generate a random number between 1 and 150
+    console.log(id); // Print the generated id
+    const finalUrl = url + id; // Combine the pokeapi url with pokemon id
+    console.log(finalUrl); // Print the final URL
+    fetch(finalUrl) // Fetch data from the final URL
+        .then((response) => response.json()) // Convert the response to JSON
+        .then((data) => {
+            generateCard(data); // Generate the card using the fetched data
+        });
+};
 
 let generateCard = (data) => {
-    // Get necessary data and assign it to variables
-    console.log(data);
-    const hp = data.stats[0].base_stat;
-    const imgSrc = data.sprites.other.dream_world.front_default;
-    const pokeName = data.name[0].toUpperCase() + data.name.slice(1);
-    const statsAttack = data.stats[1].base_stat;
-    const statDefense = data.stats[2].base_stat;
-    const statSpeed = data.stats[5].base_stat;
-
-    // set Themecolor based on pokemon type
-    const themeColor = typeColor[data.types[0].type.name];
-    console.log(themeColor);
+    console.log(data); // Print the fetched data
+    const hp = data.stats[0].base_stat; // Get the HP stat
+    const imgSrc = data.sprites.other.dream_world.front_default; // Get the image source
+    const pokeName = data.name[0].toUpperCase() + data.name.slice(1); // Get the capitalized Pokemon name
+    const statsAttack = data.stats[1].base_stat; // Get the attack stat
+    const statDefense = data.stats[2].base_stat; // Get the defense stat
+    const statSpeed = data.stats[5].base_stat; // Get the speed stat
+    const themeColor = typeColor[data.types[0].type.name]; // Get the theme color based on the pokemon type
+    console.log(themeColor); // Print the theme color
 
     card.innerHTML = `
             <p class="hp">
                 <span>HP</span>
-                ${hp}
+                ${hp} // Display the HP stat
             </p>
 
-            <img src="${imgSrc}" alt="">
+            <img src="${imgSrc}" alt=""> // Display the Pokemon image
 
-            <h2 class="poke-name">${pokeName}</h2>
+            <h2 class="poke-name">${pokeName}</h2> // Display the Pokemon name
 
             <div class="types">
                 
@@ -83,27 +75,25 @@ let generateCard = (data) => {
                 </div>
             </div>
     `;
-    appendTypes(data.types);
-    styleCards(themeColor);
+    appendTypes(data.types); // Append the Pokemon types
+    styleCards(themeColor); // Style the card using the theme color
 };
 
 let appendTypes = (types) => {
-    console.log(types);
-    types.forEach(item => {
+    console.log(types); // Print the Pokemon types
+    types.forEach((item) => {
         let span = document.createElement("span");
-        span.textContent = item.type.name;
-        document.querySelector(".types").appendChild(span);
+        span.textContent = item.type.name; // Set the text content of the span element to the type name
+        document.querySelector(".types").appendChild(span); // Append the span element to the types container
     });
 };
 
 let styleCards = (color) => {
-    card.style.background = `radial-gradient(circle at 50% 0%, ${color} 36%, #ffffff 36%)`;
-    card.querySelectorAll(".types span").forEach( typeColor => {
-        typeColor.style.backgroundColor = color;
-    })
-}
+    card.style.background = `radial-gradient(circle at 50% 0%, ${color} 36%, #ffffff 36%)`; // Set the card background color
+    card.querySelectorAll(".types span").forEach((typeColor) => {
+        typeColor.style.backgroundColor = color; // Set the background color of each type span
+    });
+};
 
-
-btn.addEventListener("click", getPokeData);
-window.addEventListener("load", getPokeData);
-
+btn.addEventListener("click", getPokeData); // Add event listener to the button
+window.addEventListener("load", getPokeData); // Fetch data when the window loads
